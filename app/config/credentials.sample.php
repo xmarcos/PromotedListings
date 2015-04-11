7fc2f17990ff23a25005e7a1374244e4ffa36614
Marcos Sader <xmarcos@gmail.com>
2015-04-11 19:22:01 -0300

    Added sample credentials files

---
+++ app/config/credentials.sample.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'facebook_app' => [
+        'app_id'       => 'APP_ID',
+        'app_secret'   => 'APP_SECRET',
+        'app_url'      => 'APP_URL',
+        'scope'        => ['ads_management', 'ads_read'],
+        'redirect_url' => 'APP_URL/settings/facebook/callback'
+    ],
+    'meli_app' => [
+        'app_id'     => 'APP_ID',
+        'app_secret' => 'APP_SECRET',
+        'app_url'    => 'APP_URL/account/login'
+    ]
+];
