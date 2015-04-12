004e3c398a45bc8ad0bb738d60ca9ae3fc499b56
Abel Barahona <abel.barahona@id90.com>
2015-04-12 00:20:02 -0300

    save data user info meli

--- app/Http/Controller/Account.php
+++ app/Http/Controller/Account.php
@@ -31,17 +31,52 @@ class Account extends BaseController
         );
 
         $auth_url = $app->config()->get('credentials.meli_app.app_url');
-
         if ($request->get('code')) {
-            $oAuth = $meli->authorize($request->get('code'), $auth_url);
-            $access_token = $oAuth['body']->access_token;
-            if ($access_token) {
-                $_SESSION['access_token'] = $access_token;
+            $oAuth = $meli->authorize($request->get('code'), $auth_url);            
+            
+            // complete meli account
+            //call to users/{user_id}
+            $dataUser = $meli->get('/users/'.$oAuth['body']->user_id);
+            
+            $meli_account['user_id'] = $oAuth['body']->user_id;
+            $meli_account['nickname'] = $dataUser['body']->nickname;
+            $meli_account['country_id'] = $dataUser['body']->country_id;
+            $meli_account['site_id'] = $dataUser['body']->site_id;
+            $meli_account['data'] = $dataUser['body'];
+            $meli_account['access_token'] = $oAuth['body']->access_token;
+            
+            if ($meli_account['access_token']) {
+                //$dataFb = $app['db']->fetchAll('SELECT * FROM facebook_access_token');
+                //$this->saveData($meli_account);
+                $table  = 'meli_account';
+                $existsUser = $app['db']->fetchAssoc("SELECT * FROM $table WHERE user_id = :user_id", array(
+                'user_id' => $meli_account['user_id']
+                ));
+                if($existsUser === false){
+                    try {
+                        $result = $app['db']->insert($table, array(
+                        'user_id' => $meli_account['user_id'],
+                        'nickname' => $meli_account['nickname'],
+                        'country_id' => $meli_account['country_id'],
+                        'site_id' => $meli_account['site_id'],
+                        'data' => json_encode($dataUser['body']),
+                    ));
+                    } catch (Exception $exc) {
+                        // We should display message error.
+                    }
+                }
+                
+                $_SESSION['access_token'] = $meli_account['access_token'];
+                $_SESSION['user_id'] = $meli_account['user_id'];
 
                 return $app->redirect($app->path('account_dashboard'));
             }
         } else {
             return new RedirectResponse($meli->getAuthUrl($auth_url));
         }
+    }
+    protected function saveData($params ,$table='meli_account'){
+        var_dump($params);die();
+        
     }
 }
