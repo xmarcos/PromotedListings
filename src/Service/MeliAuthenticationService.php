a9546f0f9dafa84f0524c0f01efe75ac6dbb166d
Marcos Sader <xmarcos@gmail.com>
2015-04-12 02:37:31 -0300

    Added new MeliAuthenticationService

---
+++ src/Service/MeliAuthenticationService.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace PromotedListings\Service;
+
+use PDO;
+use Meli;
+use DateTime;
+use Exception;
+use Doctrine\DBAL\Connection;
+use Facebook\Entities\AccessToken;
+use FacebookAds\Api;
+use FacebookAds\Object\AdUser;
+use FacebookAds\Object\Fields\AdAccountFields;
+use xmarcos\Dot\Container as DotContainer;
+use PromotedListings\Utils;
+
+class MeliAuthenticationService
+{
+    protected $db;
+    protected $meli;
+    protected $redirect_uri;
+    protected $access_token;
+
+    public function __construct(
+        Meli $meli,
+        Connection $db,
+        $redirect_uri = null
+    ) {
+        $this->db           = $db;
+        $this->meli         = $meli;
+        $this->redirect_uri = $redirect_uri;
+    }
+
+    public function getAuthUrl()
+    {
+        return $this->meli->getAuthUrl($this->redirect_uri);
+    }
+
+    public function getAccessTokenFromCode($code)
+    {
+        $response = $this->meli->authorize($code, $this->redirect_uri);
+
+        return $this->getDataFromResponse($response);
+    }
+
+    public function getUserInfoFromAccessToken(DotContainer $access_token)
+    {
+        $response = $this->meli->get('/users/' . $access_token->get('user_id'));
+
+        return $this->getDataFromResponse($response);
+    }
+
+    private function getDataFromResponse($response)
+    {
+        $data = null;
+
+        if (
+            is_array($response)
+            && isset($response['httpCode'])
+            && 200 == $response['httpCode']
+            && !empty($response['body'])
+        ) {
+            $data = DotContainer::create((array) $response['body']);
+        }
+
+        return $data;
+    }
+
+    public function getUserById($user_id)
+    {
+        $sql = 'SELECT * FROM meli_account WHERE user_id = :user_id LIMIT 1';
+        $q = $this->db->prepare($sql);
+        $q->bindValue(':user_id', $user_id);
+        $q->execute();
+
+        $user = $q->fetch();
+
+        if (isset($user['data'])) {
+            $user['data'] = json_decode($user['data'], true);
+        }
+
+        return empty($user) ? null : DotContainer::create($user);
+    }
+
+    public function saveUser(DotContainer $user_info)
+    {
+        $data = [
+            'user_id'    => (int) $user_info->get('id'),
+            'nickname'   => $user_info->get('nickname'),
+            'country_id' => $user_info->get('country_id'),
+            'site_id'    => $user_info->get('site_id'),
+            'data'       => json_encode($user_info->all()),
+            'updated'    => (new DateTime('NOW'))->format('Y-m-d H:i:s'),
+        ];
+
+        $this->db->beginTransaction();
+        try {
+            $sql = 'SELECT count(*) as users FROM meli_account WHERE user_id = :user_id';
+            $q = $this->db->prepare($sql);
+            $q->bindValue(':user_id', $data['user_id']);
+            $q->execute();
+
+            //we already have an user on that id, ignore
+            if ((int) $q->fetch(PDO::FETCH_OBJ)->users > 0) {
+                return true;
+            }
+
+            $this->db->insert('meli_account', $data);
+            $this->db->commit();
+
+            return true;
+        } catch (Exception $e) {
+            $this->db->rollback();
+            // throw $e;
+            return false;
+        }
+    }
+}
