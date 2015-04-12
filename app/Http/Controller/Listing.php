aca03e3f51489aff5c389ea0e55441eff58f5303
Julian Margara <julian-margara@users.noreply.github.com>
2015-04-12 00:15:46 -0300

    Add total items and improvements to routes of items

--- app/Http/Controller/Listing.php
+++ app/Http/Controller/Listing.php
@@ -12,6 +12,8 @@ class Listing extends BaseController {
         $controllers = $app['controllers_factory'];
 
         $controllers->get('/', [$this, 'listingIndex']);
+        $controllers->get('/search/{q}', [$this, 'listingIndex']);
+        $controllers->get('/item/{item_id}', [$this, 'listingItem'])->bind('item');
 
         return $controllers;
     }
@@ -30,7 +32,7 @@ class Listing extends BaseController {
         //Params
         $params = array(
             'access_token' => $access_token,
-            'attributes' => 'results'
+            'attributes' => 'results,paging'
         );
         if ($request->get('q')) {
             $params['q'] = $request->get('q');
@@ -39,12 +41,29 @@ class Listing extends BaseController {
         $search_response = $meli->get($path, $params);
         $items_ids = $search_response['body']->results;
         $items_ids_str = implode($items_ids);
+        $total_items = $search_response['body']->paging->total;
 
         //Get basic info of my own
-        $items_response = $meli->get('items', array('access_token' => $access_token, 'ids' => $items_ids_str, 'attributes' => 'title,subtitle,thumbnail,base_price,currency_id'));
+        $items_response = $meli->get('items', array('access_token' => $access_token, 'ids' => $items_ids_str, 'attributes' => 'id,title,subtitle,thumbnail,base_price,currency_id'));
         $items = $items_response['body'];
 
-        return $app['twig']->render('listing/index.html.twig', array('items' => $items));
+        return $app['twig']->render('listing/index.html.twig', array('items' => $items, 'total_items' => $total_items));
+    }
+
+    public function listingItem(Request $request, Application $app) {
+        $meli = new \Meli(
+                $app->config()->get('credentials.meli_app.app_id'), $app->config()->get('credentials.meli_app.app_secret'), $_SESSION['access_token']
+        );
+
+        $access_token = $_SESSION['access_token'];
+
+        if ($request->get('item_id')) {
+            $item_id = $request->get('item_id');
+            //Get basic info of my own
+            $item_response = $meli->get('items/' . $item_id, array('access_token' => $access_token, 'attributes' => 'id,title,subtitle,thumbnail,base_price,currency_id'));
+            $item = $item_response['body'];
+            return $app['twig']->render('listing/item_detail.html.twig', array('item' => $item));
+        }
     }
 
 }
