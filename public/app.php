aafcbfb466be6d1ae1f6673b09f11d78a90de992
Marcos Sader <xmarcos@gmail.com>
2015-04-11 23:54:01 -0300

    Enable error reporting

--- public/app.php
+++ public/app.php
@@ -3,6 +3,15 @@
 require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/../lib/MercadoLivre/meli.php';
 
+use Symfony\Component\Debug\Debug;
+
+error_reporting(-1);
+Debug::enable();
+
+ini_set('xdebug.var_display_max_data', -1);
+ini_set('xdebug.var_display_max_children', -1);
+ini_set('xdebug.var_display_max_depth', -1);
+
 if ('cli-server' === php_sapi_name() && isset($_SERVER['REQUEST_URI'])) {
     $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $path = urldecode($path);
